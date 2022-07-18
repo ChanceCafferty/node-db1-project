b5ec099c169c17522758f4a674ab8788851e0928
ChanceCafferty <chance.r.cafferty@gmail.com>
2022-07-18 16:39:24 -0500

    completed mvp and 2 stretch goals

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,40 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  // SELECT * FROM accounts;
+  return db('accounts');
 }
 
-const getById = id => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  // SELECT * FROM accounts WHERE id = ?
+  return db('accounts').where('id', id).first(); //.then(arrayResult => arrayResult[0]);
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+  // INSERT INTO accounts (...) VALUES (...)
+  return db('accounts').insert(account).then(arrayWithId => {
+    return getById(arrayWithId[0]);
+  });
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, account) => {
+  // UPDATE accounts SET shippername = ?, ... WHERE shipperid = ?
+  return db('accounts')
+    .where('id', id)
+    .update(account)
+    .then(numRowsAffected => {
+      if(numRowsAffected === 0) {
+        return null;
+      }
+      return getById(id);
+    });
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+const deleteById = async id => {
+  // DELETE FROM accounts WHERE id = ?
+  const result = await getById(id);
+  await db('accounts').where('id', id).delete();
+  return result;
 }
 
 module.exports = {
